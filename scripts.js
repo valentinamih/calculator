6e3817c910cbc469ee1d565974ef94d42f21bc05
Valya <vally-arrty@mail.ru>
2022-02-17 23:50:54 +0300

    refactor

--- scripts.js
+++ scripts.js
@@ -4,96 +4,77 @@ isValid = (num) => {
     }
     return true
 }
-
-
+alertIsNotValid = () => {
+    alert('enter valid number')
+}
 
 let findAreaOfSquare = () => {
-    let sideOfSquare = Number(document.getElementById('input').value);
-    isValid(sideOfSquare) ? alert('the area is ' + (sideOfSquare ** 2)) : alert('enter valid num');  
+    let sideOfSquare = Number(document.getElementById('input-square').value);
+    isValid(sideOfSquare) ? alert('the area is ' + (sideOfSquare ** 2)) : alertIsNotValid();
 }
-
-
-
 let findPerimeterOfSquare = () => {
-    let sideOfSquare = Number(document.getElementById('input').value);
-    isValid(sideOfSquare) ? alert('the perimeter is ' + (sideOfSquare * 4)) : alert('enter valid num');  
+    let sideOfSquare = Number(document.getElementById('input-square').value);
+    isValid(sideOfSquare) ? alert('the perimeter is ' + (sideOfSquare * 4)) : alertIsNotValid();
 }
-
-
-
-
 
 
 
  let findAreaOfRectangle = () => {
-    let sideOfRectangle1 = Number(document.getElementById('input1').value);
-    let sideOfRectangle2 = Number(document.getElementById('input2').value);
+     let sideOfRectangle1 = Number(document.getElementById('first-side-rectangle-input').value);
+     let sideOfRectangle2 = Number(document.getElementById('second-side-rectangle-input').value);
    if (isValid(sideOfRectangle1) && isValid(sideOfRectangle2)) {
        alert('the area of rectangle is ' + sideOfRectangle1 * sideOfRectangle2);
    } else {
-       alert('enter valid num')
+       alertIsNotValid()
    }
 }
-
 let findPerimeterOfRectangle = () => {
-    let sideOfRectangle1 = Number(document.getElementById('input1').value);
-    let sideOfRectangle2 = Number(document.getElementById('input2').value);
+    let sideOfRectangle1 = Number(document.getElementById('first-side-rectangle-input').value);
+    let sideOfRectangle2 = Number(document.getElementById('second-side-rectangle-input').value);
     if (isValid(sideOfRectangle1) && isValid(sideOfRectangle2)) {
         alert(`the perimeter of rectangle is ${sideOfRectangle1 * 2 + sideOfRectangle2 * 2}`);
     } else {
-        alert('enter valid num')
+        alertIsNotValid()
     }
  }
-
-
-
-
-
 
 
 
  let findAreaOfCircle = () => {
-    let radiusOfCircle = Number(document.getElementById('input').value);
+     let radiusOfCircle = Number(document.getElementById('input-circle').value);
     if (isValid(radiusOfCircle)) {
-        alert('the area of circle is ' + radiusOfCircle ** 2 * 3,14);
+        alert('the area of circle is ' + (radiusOfCircle ** 2 * 3.14));
     } else {
-        alert('enter valid num')
+        alertIsNotValid()
     }
  }
- 
  let findPerimeterOfCircle = () => {
-    let radiusOfCircle = Number(document.getElementById('input').value);
+     let radiusOfCircle = Number(document.getElementById('input-circle').value);
     if (isValid(radiusOfCircle)) {
-        alert('the perimeter of circle is ' + radiusOfCircle * 3,14 * 2);
+        alert('the perimeter of circle is ' + (radiusOfCircle * 3.14 * 2));
     } else {
-        alert('enter valid num')
+        alertIsNotValid()
     }
  }
-
-
-
-
-
-
 
 
  let findAreaOfTriangle = () => {
-    let baseOfTriangle = Number(document.getElementById('input4').value);
-    let heightOftriangle = Number(document.getElementById('input7').value);
-   if (isValid(baseOfTriangle) && isValid(heightOftriangle)) {
-       alert(`the area of triangle is ${heightOftriangle * baseOfTriangle / 2}`);
+     let baseOfTriangle = Number(document.getElementById('base-triangle-input').value);
+    let heightOfTriangle = Number(document.getElementById('triangle-height-input').value);
+   if (isValid(baseOfTriangle) && isValid(heightOfTriangle)) {
+       alert(`the area of triangle is ${heightOfTriangle * baseOfTriangle / 2}`);
    } else {
-       alert('enter valid num')
+       alertIsNotValid()
    }
 }
 
 let findPerimeterOfTriangle = () => {
-    let baseOfTriangle = Number(document.getElementById('input4').value);
-    let sideOfTriangle1 = Number(document.getElementById('input5').value);
-    let sideOfTriangle2 = Number(document.getElementById('input6').value);
-    if (isValid(baseOfTriangle) && isValid(sideOfTriangle1) && isValid(sideOfTriangle2)) {
-        alert(`the perimeter of triangle is ${baseOfTriangle + sideOfTriangle1 + sideOfTriangle2}`);
+    let baseOfTriangle = Number(document.getElementById('base-triangle-input').value);
+    let firstSideOfTriangle = Number(document.getElementById('first-side-triangle-input').value);
+    let secondSideOfTriangle = Number(document.getElementById('second-side-triangle-input').value);
+    if (isValid(baseOfTriangle) && isValid(firstSideOfTriangle) && isValid(secondSideOfTriangle)) {
+        alert(`the perimeter of triangle is ${baseOfTriangle + firstSideOfTriangle + secondSideOfTriangle}`);
     } else {
-        alert('enter valid num')
+        alertIsNotValid()
     }
  }
